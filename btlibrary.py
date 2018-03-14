e341513946ae4442f444175cc01520204968051d
cnaaaaa <cnaaaaa@163.com>
2018-03-14 18:32:00 +0800

    Update btlibrary.py

--- btlibrary.py
+++ btlibrary.py
@@ -1,31 +1,95 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from HTMLParser import HTMLParser
 
-from helpers import retrieve_url
 from novaprinter import prettyPrinter
 
 class BtlibraryParser(HTMLParser):
     def __init__(self, keyword):
-        try:
-            super().__init__()
-        except:
-            HTMLParser.__init__(self)
-        self.keyword = keyword
-
-        self.state
+        super().__init__()
+        self.state = self.init_state
+        self.output = {}
 
     def handle_starttag(self, tag, attrs):
-        params = dict(attrs)
+        self.state('starttag', tag, attrs)
 
     def handle_endtag(self, tag):
+        self.state('endtag', tag)
 
     def handle_data(self, data):
+        self.state('data', data)
+
+    # fsm
+    def init_state(self, step, *args):
+        if step == 'starttag' && args[0] == 'div':
+            attrs = dict(args[1])
+            if attrs.get('class') == 'item-title':
+                self.state = self.item_title_state
+            elif attrs.get('class') == 'item-detail':
+                self.state = self.item_detail_state
+
+    def item_title_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def desc_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def name_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def item_detail_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def link_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def time_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def size_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def count_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def speed_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
+
+    def leech_state(self, step, *args):
+        if step == 'starttag':
+        elif step == 'endtag':
+        elif step == 'data':
 
 
 class btlibrary(object):
     url = 'http://btlibrary.cc'
     name = 'BT Library'
+
+    def __init__(self):
+        self.parser = BtlibraryParser()
+
+    def search_test(self, data):
+        for d in self.parser.feed(data):
+            prettyPrinter(d)
 
     def search(self, what, cat='all'):
         pass
@@ -33,5 +97,7 @@ class btlibrary(object):
 
 if __name__ == "__main__":
     t = btlibrary()
-    t.search('modern+family')
+    with open('btl_src', 'rb') as f:
+        t.search_test(f.read())
+    # t.search('modern+family')
 
