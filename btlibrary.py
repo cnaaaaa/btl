4dafdf20c6c055ec98ea3a1f32c4f05a2ed2c23e
Ken <ken@Ken-MacBook-Pro.local>
2018-03-15 02:06:51 +0800

    finish parser

--- btlibrary.py
+++ btlibrary.py
@@ -4,6 +4,7 @@
 from HTMLParser import HTMLParser
 
 from novaprinter import prettyPrinter
+from helpers import retrieve_url
 
 class BtlibraryParser(HTMLParser):
     def __init__(self, keyword):
@@ -22,7 +23,7 @@ class BtlibraryParser(HTMLParser):
 
     # fsm
     def init_state(self, step, *args):
-        if step == 'starttag' && args[0] == 'div':
+        if step == 'starttag' and args[0] == 'div':
             attrs = dict(args[1])
             if attrs.get('class') == 'item-title':
                 self.state = self.item_title_state
@@ -31,53 +32,58 @@ class BtlibraryParser(HTMLParser):
 
     def item_title_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
-
-    def desc_state(self, step, *args):
-        if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            attrs = dict(args[1])
+            self.output['desc_link'] = attrs.get('href')
+            self.state = self.name_state
 
     def name_state(self, step, *args):
-        if step == 'starttag':
-        elif step == 'endtag':
+        if step == 'endtag':
+            if args[0] == 'div':
+                self.state = self.init_state
         elif step == 'data':
+            self.output['name'] = self.output.setdefault('name', '') + data
 
     def item_detail_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
-
-    def link_state(self, step, *args):
-        if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'a':
+                attrs = dict(args[1])
+                self.output['link'] = attrs.get('href')
+                self.state = self.time_state
 
     def time_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'b':
+                self.state = self.size_state
 
     def size_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'b':
+                self.state = self.size_state2
+
+    def size_state2(self, step, *args):
+        if step == 'data':
+            self.output['size'] = args[0]
+            self.state = self.count_state
 
     def count_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'b':
+                self.state = self.speed_state
 
     def speed_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'b':
+                self.state = self.leech_state
 
     def leech_state(self, step, *args):
         if step == 'starttag':
-        elif step == 'endtag':
-        elif step == 'data':
+            if args[0] == 'b':
+                self.state = self.leech_state2
+
+    def leech_state2(self, step, *args):
+        if step == 'data':
+            self.output['leech'] = data
+            self.output['seeds'] = data
 
 
 class btlibrary(object):
@@ -89,6 +95,7 @@ class btlibrary(object):
 
     def search_test(self, data):
         for d in self.parser.feed(data):
+            d['engine_url'] = url
             prettyPrinter(d)
 
     def search(self, what, cat='all'):
